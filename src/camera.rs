f788b50df94748c81e9d2f17a2696c052d360712
aliuahma <hiimaliahmad@gmail.com>
2024-12-27 18:13:07 -0800

    create camera struct and test that everything builds

--- src/camera.rs
+++ src/camera.rs
@@ -3,7 +3,23 @@ use raylib::prelude::*;
 
 pub struct CameraController {
     pub camera: Camera3D,
-    movement_speed: f32,
-    rotation_speed: f32
 }
 
+impl CameraController {
+    pub fn new() -> Self {
+        let camera = Camera3D::perspective (
+            Vector3::new(0.0, 0.0, 5.0),
+            Vector3::new(0.0, 0.0, 0.0),
+            Vector3::new(0.0, 1.0, 5.0),
+            45.0
+        );
+
+        Self {
+            camera
+        }
+    }
+
+    pub fn update(&mut self) {
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,24 @@ fn main() {
         .title("DRIVE")
         .build();
 
-    rl.set_target_fps(120);
+    rl.set_target_fps(1000);
+
+    let mut camera_controller: camera::CameraController = camera::CameraController::new();
 
     while !rl.window_should_close() {
         let mut d: RaylibDrawHandle<'_> = rl.begin_drawing(&thread);
         d.clear_background(Color::RAYWHITE);
 
-        d.draw_text("MMB to pan | Shift + MMB to rotate", 10, 40, 20, Color::BLACK);
+        camera_controller.update();
+
+        {
+            let mut d_3d = d.begin_mode3D(camera_controller.camera);
+            d_3d.draw_grid(10, 1.0);
+            d_3d.draw_cube(Vector3::new(0.0, 0.0, 0.0), 2.0, 2.0, 2.0, Color::RED);
+            d_3d.draw_sphere(Vector3::new(0.0, 3.5, 0.0), 1.0, Color::BLUE);
+        }
+
+        d.draw_text("DRIVE", 10, 40, 20, Color::BLACK);
         d.draw_fps(10, 10);
     }
 }
